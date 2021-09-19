f2ff49dae3928b7305b9802f685702b024424d7a
Mike Fabulosa <pachydermpedals@gmail.com>
2021-09-19 06:28:46 -0400

    Format indents in App.js

--- src/App.js
+++ src/App.js
@@ -6,33 +6,33 @@ import Home from './Home';
 
 class App extends React.Component {
   render() {
-      return (
-        <div className="App">
-          <div>
-            <nav>
-              <ul id="navigation">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                <Link to="/about">About</Link>
-                </li>
-                <li>
-                <Link to="/contact">Contact</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-            <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-          </Switch>
-          </div>
-            );
+    return (
+      <div className="App">
+        <div>
+          <nav>
+            <ul id="navigation">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+              <Link to="/about">About</Link>
+              </li>
+              <li>
+              <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    );
   }
 }
 
